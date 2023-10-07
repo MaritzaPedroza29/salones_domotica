50d8f472dcfe17bbed530477cf5c9ce263ee22e5
MaritzaPedroza29 <maritzapedrozacharri@gmail.com>
2023-10-06 22:19:43 -0500

    array de bloques, pisos y salones

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -2,10 +2,193 @@ import { Alert, Card, Col, Container, Row } from "react-bootstrap";
 import { Rectangulo } from "../componentes/rectaguloinicio";
 import { Cuadrado } from "../componentes/cuadradoinicio";
 import { Navegacion } from "../layouts/navegacion";
-import { Getuser } from "../services/api";
 
 function Inicio (){
-    Getuser()
+    const [salones, setSalones] = useState([
+        {
+          "bloque": "bloqueA",
+          "data": {
+            "id": 1,
+            "pisos": [
+                {
+                  "id": 1,
+                  "piso": "primer piso",
+                  "salones": [
+                    {
+                        "id":1,
+                        "Salon":"A101",
+                        "profesor":"Juan Sebastián Galindo",
+                        "estado": "0"
+                    },{
+                        "id":2,
+                        "Salon":"A102",
+                        "profesor":"Magda Martinez",
+                        "estado":"80"
+                    },{
+                        "id":3,
+                        "Salon":"A103",
+                        "profesor":"Rodolfo Ortega",
+                        "estado":"50"
+                    },{
+                        "id":4,
+                        "Salon":"A104",
+                        "profesor":"Darwin Cardozo",
+                        "estado":"0"
+                    },{
+                        "id":1,
+                        "Salon":"A105",
+                        "profesor":"Blanca Florez",
+                        "estado":"80"
+                    }
+                ]
+                },
+                {
+                  "id": 2,
+                  "piso": "segundo piso",
+                  "salones": [
+                    {
+                        "id":1,
+                        "Salon":"A206",
+                        "profesor":"Juan Sebastián Galindo",
+                        "estado":"0"
+                    },{
+                        "id":2,
+                        "Salon":"A207",
+                        "profesor":"Magda Martinez",
+                        "estado":"80"
+                    },{
+                        "id":3,
+                        "Salon":"A208",
+                        "profesor":"Rodolfo Ortega",
+                        "estado":"50"
+                    },{
+                        "id":4,
+                        "Salon":"A209",
+                        "profesor":"Darwin Cardozo",
+                        "estado":"80"
+                    },{
+                        "id":1,
+                        "Salon":"A210",
+                        "profesor":"Blanca Florez",
+                        "estado":"0"
+                    },{
+                        "id":1,
+                        "Salon":"A211",
+                        "profesor":"Giovani Rodriguez",
+                        "estado":"50"
+                    }
+                ]
+                },
+                {
+                  "id": 3,
+                  "piso": "tercer piso",
+                  "salones": [
+                    {
+                        "id":1,
+                        "Salon":"A301",
+                        "profesor":"Juan Sebastián Galindo",
+                        "estado":"0"
+                    },{
+                        "id":2,
+                        "Salon":"A302",
+                        "profesor":"Magda Martinez",
+                        "estado":"0"
+                    },{
+                        "id":3,
+                        "Salon":"A303",
+                        "profesor":"Rodolfo Ortega",
+                        "estado":"50"
+                    },{
+                        "id":4,
+                        "Salon":"A304",
+                        "profesor":"Darwin Cardozo",
+                        "estado": "0"
+                    },{
+                        "id":5,
+                        "Salon":"A305",
+                        "profesor":"Blanca Florez",
+                        "estado":"80"
+                    },{
+                        "id":6,
+                        "Salon":"A306",
+                        "profesor":"Giovani Rodriguez",
+                        "estado":"50"
+                    },{
+                        "id":7,
+                        "Salon":"A307",
+                        "profesor":"Juan Sebastián Galindo",
+                        "estado":"0"
+                    },{
+                        "id":8,
+                        "Salon":"A308",
+                        "profesor":"Rodolfo ortega",
+                        "estado":"80"
+                    },{
+                        "id":9,
+                        "Salon":"A309",
+                        "profesor":"Giovani Rodriguez",
+                        "estado":"0"
+                    }
+                ]
+                }
+              ],
+          }
+        },
+        {
+          "bloque": "bloqueB",
+          "data": {
+            "id": 2,
+            "pisos": [
+                {
+                  "id": 1,
+                  "piso": "primer piso",
+                  "salones": [
+                    {
+                        "id":1,
+                        "Salon":"B102",
+                        "profesor":"Administrativo",
+                        "estado":"administrativo"
+                    },]
+                },{
+                    "id": 2,
+                    "piso": "segundo piso",
+                    "salones": [
+                      {
+                          "id":1,
+                          "Salon":"B201",
+                          "profesor":"Rodolfo Ortega",
+                          "estado":"50"
+                      },{
+                        "id":2,
+                        "Salon":"B202",
+                        "profesor":"Darwin Cardozo",
+                        "estado":"0"
+                    },{
+                        "id":3,
+                        "Salon":"B203",
+                        "profesor":"Giovani Rodriguez",
+                        "estado":"50"
+                    },{
+                        "id":4,
+                        "Salon":"B204",
+                        "profesor":"Magda Martinez",
+                        "estado":"80"
+                    },] 
+                }
+            ]
+          }
+        },
+        {
+          "bloque": "bloqueC",
+          "data": {
+            "id": 3,
+            "imagen": "./img/profesora3.png",
+            "nombre": "Magda Martinez",
+            "correo": "doc_mf_martinez@fesc.edu.co",
+            "estado": "1"
+          }
+        }
+      ]);
     return(
         <>
             <Navegacion></Navegacion>
